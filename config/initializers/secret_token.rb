f544508a83dd861235e8cfb83bd5d6f472e01924
kannancet <krxcet@gmail.com>
2013-12-12 10:47:06 +0530

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Awstest::Application.config.secret_key_base = '072e1ef8d57f482927055b053a436fee5c72a28161f9bebcf455be93f0b31cd8b4b8dce45b1b556a741bceb1b9152ad17aad4b38242272ca8a572410a9e70508'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Awstest::Application.config.session_store :cookie_store, key: '_awstest_session'
